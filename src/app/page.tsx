093e26b7e4cf7aa8df0c448db564dd046361752b
Akshay Gupta <akshaygupta0795@gmail.com>
2024-11-03 23:06:52 +0530

    [wip] add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { YoutubeResponse, YoutubeVideo } from '@/app/types';
 
 export default function Home() {
   const [videos, setVideos] = useState<YoutubeVideo[]>([]);
+  const [selectedVideo, setSelectedVideo] = useState<YoutubeVideo | null>(null);
 
   useEffect(() => {
     const loadVideos = async () => {
@@ -16,18 +17,19 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <main>
-        {videos.length > 0 ? (
-          <div>
-            {videos.map(video => (
-              <div key={video.id.videoId}>{video.snippet.title}</div>
-            ))}
+    <div className='flex flex-col md:flex-row min-h-screen bg-gray-50'>
+      <main className='fflex-1 md:flex md:items-center md:justify-center order-1 md:order-2'>
+        {selectedVideo ? (
+          <div className='w-full max-w-5xl p-4 md:p-8'>
+            Selected Video
           </div>
         ) : (
-            <div>No videos found</div>
+          <div className='text-gray-500 p-4'>
+            <p>Select a video to get started</p>
+          </div>
         )}
       </main>
+      <div className='flex flex-col min-h-[400px] md:h-screen flex-1 md:flex-none w-full md:w-[350px] bg-white shadow-lg order-2 md:order-1'>Sidebar</div>
     </div>
   );
 }
