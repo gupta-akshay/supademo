fa01902491a87d00555bf2cd940a637bdc9b3525
Akshay Gupta <akshaygupta0795@gmail.com>
2024-11-03 22:37:45 +0530

    [setup] generate types for data

---
+++ src/app/types.ts
@@ -0,0 +1,43 @@
+export interface YoutubeResponse {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  regionCode: string;
+  pageInfo: PageInfo;
+  items: YoutubeVideo[];
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface YoutubeVideo {
+  kind: string;
+  etag: string;
+  id: {
+    kind: string;
+    videoId?: string;
+    channelId?: string;
+  };
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: Thumbnail;
+      medium: Thumbnail;
+      high: Thumbnail;
+    };
+    channelTitle: string;
+    liveBroadcastContent: string;
+    publishTime: string;
+  };
+}
+
+interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
